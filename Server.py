52afd85709761802b873989c07c6ffd94220db54
Adam Ohlsén <ohlsadam09@gmail.com>
2026-03-14 19:42:58 +0100

    Figured out socket

--- Server.py
+++ Server.py
@@ -1,17 +1,41 @@
 import socket
 from os import environ
+import threading
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 Capacity = 4
 
-server.bind(('127.0.0.1',6677))
-server.listen(Capacity)
+
+#reminder to future self: (and roman i guess)
+# If you use firewall it will not be able to recieve anything
+clients = []
+def Check_for_connections():
+    while True:
+        global clients
+        client,addr = server.accept()
+        clients.append((client,addr))
+
+
+server.bind(('192.168.0.49',6677))
+print("server bound")
 
 print (server)
-client = None
 
-while client == None:
-    client, addr = server.accept()
+
+
+server.listen()
+
+Connection_thread = threading.Thread(target=Check_for_connections)
+Connection_thread.start()
+
+print("pluh")
+
+clients_dict = {
+
+}
+
 
 while True:
-    print(client.recv(1024).decode())
+    for client in clients:
+        print(client[0].recv(1024).decode())
+
--- client.py
+++ client.py
@@ -1,18 +1,26 @@
 import socket
-from os import environ
+from os import environ,getenv
+from dotenv import load_dotenv,dotenv_values
+from time import sleep
+
+load_dotenv()
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-Server_ip = environ.get('Server_ip',None)
+Server_ip = getenv('Server_ip')
 
-if Server_ip != None:
+Connected = False
+while Connected == False:
     try:
-        client.connect(Server_ip,6677)
+        client.connect((Server_ip,6677))
     except ConnectionRefusedError:
         print("Server refused connection")
     except ConnectionAbortedError:
-        print("Timedout, server not found")
+        print("Server had an abortion")
     except ConnectionError:
         print("Idk, some error in connection")
+    except:
+        print("damn, i don't know what error you got?")
+    sleep(0.5)
 
 
 while True:
